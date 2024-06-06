45f279d71f592f3f2337886aaf345ecd96ea0c91
LanSean1120 <lansean1120@gmail.com>
2024-06-06 13:30:48 +0800

    Time: 305 ms (6.11%), Space: 12.5 MB (5.49%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def subsetsWithDup(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        nums.sort()
+        f ={}
+        def sol(l):
+            if l==[]:
+                return []
+            if tuple(l) in f:
+                return f[tuple(l)]
+            pos = []
+            for i in range(len(l)):
+                temp = l[:i]+l[i+1:]
+                for j in range(len(sol(temp))):
+                    if sol(temp)[j] not in pos:
+                        pos.append(sol(temp)[j])
+            f[tuple(l)]= [l] + pos
+            return f[tuple(l)]
+        return sol(nums)+ [[]]
+        
