a6d766e0e00e44aba23a89194b461c01191f56f6
Sean Lan <lansean1120@gmail.com>
2024-07-07 08:50:45 +0800

    Time: 3 ms (13.51%), Space: 5.3 MB (54.05%) - LeetHub

---
+++ 1642-water-bottles/1642-water-bottles.c
@@ -0,0 +1,9 @@
+int numWaterBottles(int numBottles, int numExchange) {
+    int ans = numBottles;
+    int empty = numBottles;
+    while(empty >= numExchange){
+        ans += empty/numExchange;
+        empty = empty/numExchange + empty%numExchange;
+    }
+    return ans;
+}
