7e1905f7949f245c9295136f3a6c7ca6f9ede796
Sean Lan <lansean1120@gmail.com>
2024-07-07 08:49:04 +0800

    Time: 28 ms (90.14%), Space: 16.6 MB (32.78%) - LeetHub

---
+++ 1642-water-bottles/1642-water-bottles.py
@@ -0,0 +1,9 @@
+class Solution:
+    def numWaterBottles(self, numBottles: int, numExchange: int) -> int:
+        ans = numBottles
+        empty = numBottles
+        
+        while empty >= numExchange:
+            ans += empty//numExchange
+            empty = empty//numExchange + empty%numExchange
+        return ans
