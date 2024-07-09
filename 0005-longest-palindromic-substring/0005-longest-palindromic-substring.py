013e1a37a8f9e95a321dd8af4ef29da328033036
Sean Lan <lansean1120@gmail.com>
2024-07-09 13:48:12 +0800

    Time: 531 ms (47.53%), Space: 16.5 MB (64.65%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,16 +1,18 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        candidates = [s[0]]
+        ans = s[0]
         for i in range(len(s)):
             j = 0
             while i+j<len(s) and i-j >= 0:
                 if s[i+j] != s[i-j]:
                     if i-j+1 >= i+j-1 :
                         break
-                    candidates.append(s[i-j+1:i+j-1 +1])
+                    if len(s[i-j+1:i+j-1 +1]) > len(ans):
+                        ans = s[i-j+1:i+j-1 +1]
                     break
                 if (i+j == len(s)-1 or i-j == 0) and s[i+j] == s[i-j]:
-                    candidates.append(s[i-j:i+j +1])
+                    if len(s[i-j:i+j +1]) > len(ans):
+                        ans = s[i-j:i+j +1]
                 j+=1
 
         for k in range(len(s)-1):
@@ -18,13 +20,12 @@ class Solution:
                 j = 0
                 while k+1+j <len(s) and k-j >= 0:
                     if s[k+1+j] != s[k-j]:
-                        candidates.append(s[k-j+1: k+1+j-1+1])
+                        if len(s[k-j+1: k+1+j-1+1]) > len(ans):
+                            ans = s[k-j+1: k+1+j-1+1]
                         break
                     if (k+1+j == len(s)-1 or k-j==0) and s[k+1+j] == s[k-j]:
-                        candidates.append(s[k-j:k+1+j+1])
+                        if len(s[k-j:k+1+j+1]) > len(ans):
+                            ans = s[k-j:k+1+j+1]
                     j+=1
-        ans = candidates[0]
-        for elem in candidates:
-            if len(elem) > len(ans):
-                ans = elem
+
         return ans
