d88e15a6beb2cfc1d456c563eaeae1919133ae18
Sean Lan <lansean1120@gmail.com>
2024-07-15 21:43:15 +0800

    Time: 113 ms (97.01%), Space: 25.4 MB (25.75%) - LeetHub

--- 0335-self-crossing/0335-self-crossing.py
+++ 0335-self-crossing/0335-self-crossing.py
@@ -8,12 +8,12 @@ class Solution:
                     return False
             return True
 
-        if distance == [1,1,2,1,1]:
-            return True
         
         for i in range(len(distance)-2):
             if distance[i+2] - distance[i] <=0:
                 if i-2>=0 and distance[i-2] + distance[i+2] >= distance[i] and i+3 <= len(distance)-1 and distance[i+3] + distance[i-1] >= distance[i+1]:
+                    return True
+                if i==1 and i+3<=len(distance)-1 and distance[i] == distance[i+2] and distance[i+3] + distance[i-1] >= distance[i+1]:
                     return True
                 if not spiraling_in(distance[i:]):
                     return True
