b094a1595c012cb5783ab46b910af835ee85435f
Sean Lan <lansean1120@gmail.com>
2024-07-22 12:37:11 +0800

    Time: 2688 ms (14.12%), Space: 27.8 MB (24.86%) - LeetHub

--- 2101-last-day-where-you-can-still-cross/2101-last-day-where-you-can-still-cross.py
+++ 2101-last-day-where-you-can-still-cross/2101-last-day-where-you-can-still-cross.py
@@ -1,22 +1,39 @@
 class Solution:
     def latestDayToCross(self, row: int, col: int, cells: List[List[int]]) -> int:
-        
-        cells = list(map(tuple, cells))
-        
-        def search(day):
-            queue = deque([(1,i) for i in range(1, col+1)])
-            visited = set(cells[:day])
+        def neighbor(cell):
+            i, j = cell
+            res = []
+            if i - 1 >= 1:
+                res.append([i - 1, j])
+            if i + 1 <= row:
+                res.append([i + 1, j])
+            if j - 1 >= 1:
+                res.append([i, j - 1])
+            if j + 1 <= col:
+                res.append([i, j + 1])
+            return res
+
+        def check(n):
+            blocked = set(map(tuple, cells[:n]))
+            queue = deque([(1, i) for i in range(1, col + 1) if (1, i) not in blocked])
+            visited = set(queue)
+            
             while queue:
-                r, c = queue.popleft()
-                if (r,c) in visited:
-                    continue
-                visited.add((r, c))
-                if not (1 <= r <= row and 1 <= c <= col):
-                    continue
-                if r == row:
-                    return 0
-                for dr, dc in ((0, 1), (1, 0), (-1, 0), (0, -1)):
-                    queue.append((r+dr,  c+dc))
-            return 1
+                curr_i, curr_j = queue.popleft()
+                if curr_i == row:
+                    return True
+                for ni, nj in neighbor((curr_i, curr_j)):
+                    if (ni, nj) not in blocked and (ni, nj) not in visited:
+                        queue.append((ni, nj))
+                        visited.add((ni, nj))
+            return False
         
-        return bisect.bisect(range(1, len(cells)), 0, key=search)
+        left, right = 1, row * col
+        while left < right:
+            mid = (left + right + 1) // 2
+            if check(mid):
+                left = mid
+            else:
+                right = mid - 1
+        
+        return left
