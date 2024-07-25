772123fdf2cc4c34a042ae8a2277e3ec2679de07
Sean Lan <lansean1120@gmail.com>
2024-07-25 21:32:33 +0800

    Time: 1125 ms (53.45%), Space: 25.4 MB (33.96%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.py
+++ 0948-sort-an-array/0948-sort-an-array.py
@@ -4,30 +4,30 @@ class Solution:
             res = []
             i = 0
             j = 0
-            while i < len(left) and j < len(right):
+            while i<len(left) and j<len(right):
                 if left[i] <= right[j]:
                     res.append(left[i])
-                    i += 1
+                    i+=1
                 else:
                     res.append(right[j])
-                    j += 1
-            while i < len(left):
+                    j+=1
+
+            while i<len(left):
                 res.append(left[i])
                 i+=1
-            while j < len(right):
+            while j<len(right):
                 res.append(right[j])
-                j +=1
+                j+=1
             return res
         
         def parti(nums):
-            if len(nums) <=1:
+            if len(nums) <= 1:
                 return nums
+            
             mid = (len(nums)+1)//2
+
             left = nums[:mid]
             right = nums[mid:]
-
             return merge(parti(left), parti(right))
-
+        
         return parti(nums)
-
-                
